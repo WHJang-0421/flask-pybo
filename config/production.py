f866fab96ed3bf9aa00c75a93b6afb3600503b90
WoohyukJang <morrranii@gmail.com>
2023-02-02 22:24:13 +0900

    Add Logging

--- config/production.py
+++ config/production.py
@@ -1,5 +1,29 @@
 from config.default import *
+from logging.config import dictConfig
 
 SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(BASE_DIR, "pybo.db")}'
 SQLALCEHMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'[*pf\x81)\xbd\xbb\x16pL\xe2\xd5\xfd\x17\xee'
+
+dictConfig({
+    'version': 1,
+    'formatters': {
+        'default': {
+            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/flask-pybo.log'),
+            'maxBytes': 1024 * 1024 * 5, # 5MB
+            'backupCount': 5,
+            'formatter': 'default',
+        },
+    },
+    'root': {
+        'level': 'INFO',
+        'handlers': ['file'],
+    },
+})

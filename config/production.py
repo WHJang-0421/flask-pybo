07f594d1288e0544c995f9c671b4f3903fe4ebbc
WoohyukJang <morrranii@gmail.com>
2023-02-04 15:16:08 +0900

    Fix Typo production.py

--- config/production.py
+++ config/production.py
@@ -4,7 +4,11 @@ from dotenv import load_dotenv
 
 load_dotenv(os.path.join(BASE_DIR, '.env'))
 
-SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg2://{os.getenv("DB_USER")}:{os.getenv("DB_PASSWORD")}@{os.getenv("DB_HOST")/{os.getenv("DB_NAME")}}'
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
+    user=os.getenv('DB_USER'),
+    pw=os.getenv('DB_PASSWORD'),
+    url=os.getenv('DB_HOST'),
+    db=os.getenv('DB_NAME'))
 SQLALCEHMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'[*pf\x81)\xbd\xbb\x16pL\xe2\xd5\xfd\x17\xee'
 
